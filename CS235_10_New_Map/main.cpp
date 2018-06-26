3aab5665d07b96bcb43ab72d2212879b064f933e
kiyama14 <kiy11001@byui.edu>
2018-06-26 11:31:28 -0600

    Initial Commit

---
+++ CS235_10_New_Map/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS235_10_New_Map
+//
+//  Created by Mauricio Takashi Kiyama on 6/26/18.
+//  Copyright © 2018 Marcio Kazuo Kiyama. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
