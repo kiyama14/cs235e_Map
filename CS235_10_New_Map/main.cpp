21e86567e6af317970acb51e1c141891b8a99e52
kiyama14 <kiy11001@byui.edu>
2018-06-26 12:28:50 -0600

    first commit

--- CS235_10_New_Map/main.cpp
+++ CS235_10_New_Map/main.cpp
@@ -7,9 +7,62 @@
 //
 
 #include <iostream>
+#include <map>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+using namespace std;
+typedef map<string, int> Map;
+typedef map<string, int>::reverse_iterator MapIterator;
+typedef multimap<int, string>::reverse_iterator MultiMapIterator;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    
+    string userInput;
+    int numberOfWords = 0;
+    
+    cout << "Enter filename: ";
+    cin >> userInput;
+    
+    ifstream fin;
+    fin.open(userInput.c_str());
+    
+    if (fin.fail()) {
+        cout << "Error: Unable to open file";
+        return 0;
+    }
+    
+    Map myMap;
+    string word;
+    
+    while (fin >> word) {
+        
+        transform(word.begin(), word.end(), word.begin(), ::tolower);
+        string noPunWord = "";
+        
+        for (int i = 0; i < word.length(); i++) {
+            if (!ispunct(word[i]) || word[i] != '-')
+                noPunWord += word[i];
+        }
+        
+        myMap[noPunWord]++;
+        numberOfWords++;
+    }
+
+    
+    fin.close();
+    
+
+    cout << "Number of words processed: " << numberOfWords;
+    
+    multimap<int, string> megaMap;
+    
+    
+    
+    
+    
+    
     return 0;
 }
